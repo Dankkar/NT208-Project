d268ec4684c8a9fa95e672325cc9993ee91882f8
h4rrydabrwn <huanphan2497@gmail.com>
2025-04-26 16:31:19 +0700

    Chuc nang quen mat khau + API gui Mail Reset password + Reset pass bang token

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,13 +1,15 @@
 //backend/src/routes/authRoutes.js
 const express = require('express');
 const {check, validationResult} = require('express-validator');
-const {register} = require('../controllers/authController');
+const {register, forgotPassword, resetPassword} = require('../controllers/authController');
+
 const router = express.Router();
 
+// Dang ky
 router.post(
     '/register',
     [
-        check('HoTen', 'Họ tên không được để trống').notEmpty(),,
+        check('HoTen', 'Họ tên không được để trống').notEmpty(),
         check('Email', 'Email không hợp lệ').isEmail(),
         check('MatKhau', 'Mật khẩu tối thiểu 8 ký tự').isLength({ min: 8 }),    
         
@@ -19,6 +21,35 @@ router.post(
         next();
     },
     register
+);
+
+// Quen mat khau
+router.post(
+    '/forgot-password',
+    [
+        check('Email', 'Email khong hop le').isEmail(),
+    ],
+    (req, res, next) => {
+        const errs = validationResult(req);
+        if(!errs.isEmpty()) return res.status(400).json({errors: errs.array() });
+        next();
+    },
+    forgotPassword
+);
+
+// Dat lai mat khau
+router.post(
+    '/reset-password',
+    [
+        check('token', 'Token khong duoc de trong').notEmpty(),
+        check('newPassword', 'Mat khau toi thieu 8 ky tu').isLength({ min: 8}),
+    ],
+    (req, res, next) => {
+        const errs = validationResult(req);
+        if(!errs.isEmpty()) return res.status(400).json({ errors: errs.array() });
+        next();
+    },
+    resetPassword
 );
 
 module.exports = router;
