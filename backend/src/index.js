b009a6147a37ac55e8ebebe148c598a2f3409584
h4rrydabrwn <huanphan2497@gmail.com>
2025-05-16 23:34:27 +0700

    Them admin (adminController, adminRoute), sua ham Login luc dang nhap truyen them LoaiUser de xac thuc trong middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,10 +24,13 @@ const authRoutes = require('./routes/authRoutes');
 const bookingRoutes = require('./routes/bookingRoutes')
 const hotelRoutes = require('./routes/hotelRoutes');
 const roomTypeRoutes = require('./routes/roomTypeRoutes');
+const adminRoutes = require('./routes/adminRoutes');
+
 app.use('/api/auth', authRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/hotels', hotelRoutes);
 app.use('/api/roomTypes', roomTypeRoutes);
+app.use('/api/admin', adminRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
