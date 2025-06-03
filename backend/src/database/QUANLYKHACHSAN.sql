db72c87359587224ba6b124cea9658f7b34ffa79
kha <cube71kd@gmail.com>
2025-06-03 13:23:44 +0700

    them cot link anh cho loaiphong

--- backend/src/database/QUANLYKHACHSAN.sql
+++ backend/src/database/QUANLYKHACHSAN.sql
@@ -78,9 +78,11 @@ CREATE TABLE LoaiPhong (
     CONSTRAINT FK_LoaiPhong_KhachSan FOREIGN KEY (MaKS) REFERENCES KhachSan(MaKS) ON DELETE CASCADE -- Nếu KS bị xóa, các loại phòng cũng bị xóa
 );
 
+
 ALTER TABLE LoaiPhong
 ADD IsActive BIT DEFAULT 1 NOT NULL
 
+ALTER TABLE LoaiPhong ADD DuongDanAnh NVARCHAR(500) NULL;
 -- Bảng Cấu Hình Giường (Bed Configuration)
 CREATE TABLE CauHinhGiuong (
     MaCauHinhGiuong INT PRIMARY KEY IDENTITY(1,1),  -- Mã cấu hình giường (PK)
@@ -402,3 +404,11 @@ ADD MaKhach INT NULL,
 select * from LoaiPhong
 SELECT * FROM NguoiDung
 select * from LoaiDichVu
+-- Chạy trong SQL Server Management Studio
+INSERT INTO AnhKhachSan (MaKS, TenFile, DuongDanAnh, LoaiAnh, ThuTu, MoTa)
+VALUES 
+(1, 'test1.jpg', 'uploads/hotels/test1.jpg', 'main', 1, 'Ảnh chính khách sạn'),
+(1, 'test2.jpg', 'uploads/hotels/test2.jpg', 'gallery', 2, 'Ảnh gallery 1'),
+(1, 'test3.jpg', 'uploads/hotels/test3.jpg', 'gallery', 3, 'Ảnh gallery 2');
+
+SELECT * FROM AnhKhachSan
